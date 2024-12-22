ad7baa98d432ff3e86e9fcb930494de1c0f0424f
gleidson <gleidsonlimacrf@gmail.com>
2024-12-22 01:03:42 -0300

    curso de js do matheus battisti

---
+++ 3_ESTRUTURAS/js/script.js
@@ -0,0 +1,45 @@
+// 1 - variáveis
+let nome = "Gleidson"
+
+console.log(nome);
+
+nome = "Gleidson Lima";
+
+console.log(nome)
+
+const idade = 17
+
+console.log(idade)
+
+// 2 - mais sobre variáveis
+
+// let 2teste = "inválido"
+// let @teste = "inválido"
+
+let a = 10,
+b = 20,
+c = 30;
+
+console.log(a, b, c);
+
+const nomecompleto = "Matheus Battisti";
+const nomeCompleto = "Gleidson Lima";
+
+console.log(nomecompleto);
+console.log(nomeCompleto);
+
+
+// 3 - prompt
+const cuida = prompt("digite seu nome, coração!");
+
+console.log(`Seja bem vindo ${cuida}`);
+
+
+// 4 - alert
+alert(`Seja bem vindo!`)
+let Name = prompt("Digite seu nome:")
+
+console.log(`Sejá bem vindo ${Name}`)
+
+// 5 - Math
+console.log(Math.max(5, 2, 1, 10))
